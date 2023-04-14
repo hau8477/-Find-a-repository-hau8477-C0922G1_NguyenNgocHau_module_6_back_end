038c0af0768a703adffced89159211037b9c85a3
hau8477 <hau8477@gmail.com>
2023-04-14 10:50:21 +0700

    up

--- src/demo-websocket-notification/src/main/java/com/example/demowebsocketnotification/NotificationHandler.java
+++ src/demo-websocket-notification/src/main/java/com/example/demowebsocketnotification/NotificationHandler.java
@@ -1,29 +1,37 @@
-//package com.example.demowebsocketnotification;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.messaging.simp.SimpMessagingTemplate;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.socket.TextMessage;
-//import org.springframework.web.socket.WebSocketSession;
-//import org.springframework.web.socket.handler.TextWebSocketHandler;
-//
-//import java.io.IOException;
-//
-//@Component
-//public class NotificationHandler extends TextWebSocketHandler {
-//    private final SimpMessagingTemplate template;
-//
-//    @Autowired
-//    public NotificationHandler(SimpMessagingTemplate template) {
-//        this.template = template;
-//    }
-//
+package com.example.demowebsocketnotification;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+
+@RestController
+@CrossOrigin
+public class NotificationHandler extends TextWebSocketHandler {
+    @Autowired
+    private SimpMessagingTemplate template;
+
 //    @Override
 //    public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
-//        ObjectMapper mapper = new ObjectMapper();
-//        Notification notification = mapper.readValue(message.getPayload(), Notification.class);
+////        ObjectMapper mapper = new ObjectMapper();
+////        Notification notification = mapper.readValue(message.getPayload(), Notification.class);
 //        this.template.convertAndSend("/topic/notification", notification);
 //    }
-//}
-//
+
+    @GetMapping("/notify")
+    public String getNotification() {
+
+        // Push notifications to front-end
+        template.convertAndSend("/topic/notification", "ĐÃ GỬI THÔNG BÁO");
+
+        return "Notifications successfully sent to Angular !";
+    }
+}
+
---
+++ src/sql.sql
@@ -0,0 +1,45 @@
+CREATE TABLE patients (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  dob DATE NOT NULL,
+  gender ENUM('Male', 'Female', 'Other') NOT NULL,
+  phone VARCHAR(20) NOT NULL,
+  address VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE doctors (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  specialization VARCHAR(50) NOT NULL,
+  phone VARCHAR(20) NOT NULL,
+  email VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE appointments (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  patient_id INT(6) UNSIGNED NOT NULL,
+  doctor_id INT(6) UNSIGNED NOT NULL,
+  appointment_date DATETIME NOT NULL,
+  description TEXT,
+  FOREIGN KEY (patient_id) REFERENCES patients(id),
+  FOREIGN KEY (doctor_id) REFERENCES doctors(id)
+);
+
+CREATE TABLE prescriptions (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  patient_id INT(6) UNSIGNED NOT NULL,
+  doctor_id INT(6) UNSIGNED NOT NULL,
+  appointment_id INT(6) UNSIGNED NOT NULL,
+  medicine VARCHAR(100) NOT NULL,
+  dosage VARCHAR(20) NOT NULL,
+  duration VARCHAR(20) NOT NULL,
+  FOREIGN KEY (patient_id) REFERENCES patients(id),
+  FOREIGN KEY (doctor_id) REFERENCES doctors(id),
+  FOREIGN KEY (appointment_id) REFERENCES appointments(id)
+);
+
+CREATE TABLE departments (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
